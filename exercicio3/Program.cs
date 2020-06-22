0ee3f2d6c48b435efac4eb2ce7d589010124fb28
leonardomatumona <leonardomatumona@gmail.com>
2020-06-22 16:57:41 -0300

    Add project files.

---
+++ exercicio3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float numeroDeFuncionarios, horaDeTrabalho, valorDaHora, salarioDoFuncionario;
+
+           
+         
+            Console.WriteLine("Numero de Funcionários:");
+            numeroDeFuncionarios = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Valor da Hora:");
+            valorDaHora = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Salário do Funcionário:");
+            salarioDoFuncionario = float.Parse(Console.ReadLine());
+           
+
+            Console.WriteLine();
+            Console.WriteLine(salarioDoFuncionario.ToString("f2"));
+        }
+    }
+}
